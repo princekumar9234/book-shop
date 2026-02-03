93e1b2c9af945eb8cee8c6fb233124bf1592a4d1
Prince kumar <princecchouhan754288@gmail.com>
2026-02-03 23:09:47 +0530

    Fix User model pre-save hook bug

--- models/User.js
+++ models/User.js
@@ -31,10 +31,11 @@ const UserSchema = new mongoose.Schema({
 // Password Hash Middleware
 UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 // Compare Password Method
